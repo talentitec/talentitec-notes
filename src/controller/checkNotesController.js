465287f7cbf04a4438c84ed7bf19ed41e9f698e9
talentitec <talentitec@gmail.com>
2024-07-19 14:48:17 -0300

    Changes to be committed: 	new file:   src/controller/checkNotesController.js

---
+++ src/controller/checkNotesController.js
@@ -0,0 +1,41 @@
+const Notes = require('../model/notesModel');
+const Moment = require('moment-timezone');
+const Yup = require('yup');
+
+module.exports = { 
+
+    async update(req, res) {
+
+        const schema = Yup.object({
+            check: Yup.boolean().required()
+        });
+
+        if (!(await schema.isValid(req.body)))
+            return res.status(400).json({ success: false, message: "Campos obrigatórios não foram preenchidos!" });
+
+        const updateAt = Moment().tz("America/Sao_Paulo").format();
+        const check = req.body.check;
+        const company_id = req.company_id;
+        const employee_id = req.user_id;
+        const _id = req.params.id;
+
+        try {
+  
+            const responseNotes = await Notes.findOneAndUpdate({ _id, company_id, employee_id }, {
+                updateAt,
+                check
+            });
+
+            if(!responseNotes)
+                return res.status(400).json({ success: false, message: "Falha ao atualizar nota!" });
+        
+            return res.status(200).json({ success: true, message: "Nota atualizado com sucesso!" });
+            
+        } catch (error) {
+
+            console.log(`Erro: ${error}`)
+
+            return res.status(400).json({ success: false, message: "Falha ao atualizar nota!" });
+        }
+    },
+}
